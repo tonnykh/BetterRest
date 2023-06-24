1ebbcef6fefd0f6f325cb90a7ea37d24059e09a9
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-24 10:45:51 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Khumbongmayum Tonny on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
